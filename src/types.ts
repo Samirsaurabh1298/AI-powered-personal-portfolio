ebe44e0cbdf0e7f2468f56caa78e885379bb891c
Samir Saurabh <samirverma527@gmail.com>
2026-06-28 13:02:16 +0530

    chore: add TypeScript support -- packages, tsconfig, shared types

---
+++ src/types.ts
@@ -0,0 +1,28 @@
+export interface Message {
+  role: 'user' | 'bot'
+  text: string
+  time: string
+}
+
+export interface ChatContextType {
+  messages: Message[]
+  sendMessage: (text: string) => Promise<void>
+  isStreaming: boolean
+  clearChat: () => void
+}
+
+export interface ThemeContextType {
+  isDark: boolean
+  toggleTheme: () => void
+}
+
+export interface AnimatedStatProps {
+  value: number
+  suffix: string
+  label: string
+  delay: number
+}
+
+export interface TypewriterTextProps {
+  text: string
+}
